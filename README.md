52c33e37ea44ba1a6627ad639b04f00d3d379a52
Steam Deck User <workingmanrob@gmail.com>
2025-12-11 02:05:47 -0500

    update readme - add sdcard folder

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # geeni-aware
+
+Attempt to hack a Geeni branded ip camera.
+
+sdcard folder contains the bare minimum required to setup wifi and run dropbear ssh server.  The root password will be set to cosmicpower - yes that is a Genie joke.
+
+Update wifi_cfg.ini to include your wireless ssid and password.
---
+++ sdcard/myupdate.sh
@@ -0,0 +1,28 @@
+#! /bin/sh
+
+# Backup the original contents of /etc/jffs2 to the sdcard
+if [[ ! -d /mnt/jffs2 ]]; then
+	cp -a /etc/jffs2 /mnt/
+fi
+
+if [[ ! -f /mnt/bak/shadow ]]; then
+	# move modified shadow file to set root password = cosmicpower
+	mv /etc/jffs2/shadow /etc/jffs2/shadow.org
+	if [[ ! -d /mnt/bak ]]; then
+		mkdir /mnt/bak
+	fi
+	cp /mnt/shadow /mnt/bak/
+	mv /mnt/shadow /etc/jffs2/
+fi
+
+/mnt/wifi_manage.sh start_sta
+
+sleep 10
+
+/mnt/dropbear/dropbearmulti dropbear -r /mnt/dropbear/dropbear_ecdsa_host_key &
+
+sleep 10
+
+while true; do
+	sleep 600
+done
